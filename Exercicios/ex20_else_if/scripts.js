00f6491cd3ec84646b35d533c950b0ecf08ea804
Carlos Eduardo <carlosggomes2017@gmail.com>
2022-12-20 13:05:23 -0300

    meus estudos

---
+++ Exercicios/ex20_else_if/scripts.js
@@ -0,0 +1,34 @@
+var nome = "Pedro";
+
+if(nome == "Pedro") {
+  console.log("O nome dele é Pedro");
+} 
+else if(nome == "Matheus") {
+  console.log("O nome é Matheus");
+} 
+else if(nome == "Xavier") {
+  console.log("O nome é Xavier");
+} 
+else {
+  console.log("Ele possui outro nome!");
+}
+
+
+
+var idade = 18;
+
+if(idade > 20) {
+  console.log("Ele pode entrar na festa!");
+} 
+
+else if(idade >= 18) {
+  console.log("Ele só pode entrar com autorização");
+}
+
+if(nome == "Matheus") {
+  console.log("teste");
+} 
+
+else {
+  console.log("testando");
+}
---
+++ Exercicios/ex23_while/scripts.js
@@ -0,0 +1,22 @@
+var x = 0;
+
+while (x < 5){
+
+    console.log("testando repeticao" + x);
+    
+    //incrementador
+
+    x++;
+}
+
+var arr = ["teste","testando","a","b"];
+
+var i = 6
+
+while (i<=3){
+
+    console.log(arr[y])
+
+    i++;
+}
+
