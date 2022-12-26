0c4861f2ef2331acbf3b20b85a83020b86bd2509
Carlos Eduardo <carlosggomes2017@gmail.com>
2022-12-26 14:00:08 -0300

    estudos

---
+++ Exercicios/ex27_function/scripts.js
@@ -0,0 +1,35 @@
+function primeiraFuncao(){
+
+  console.log("hello word das funcoes")
+}
+
+primeiraFuncao();
+
+function dizerNome(nome){
+
+  console.log("O nome é:" + nome);
+
+}
+
+dizerNome("mathues")
+dizerNome("pedro")
+dizerNome("xavier")
+
+var nomeDoBancoDeDados = "joao";
+
+dizerNome(nomeDoBancoDeDados);
+
+function soma(a, b) {
+  var soma = a + b;
+  return soma;
+}
+
+var somaUm = soma(2, 5);
+
+console.log(somaUm);
+
+var somaDois = soma(5, 5);
+
+console.log(somaDois);
+
+console.log(soma(4, 5));
